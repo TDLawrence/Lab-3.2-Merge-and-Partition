117c44590e486ba30518bb760292a851a161dd74
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-02-06 08:59:52 -0500

    Completed partitionSort

--- Lab 3.2/src/sortHelpers.java
+++ Lab 3.2/src/sortHelpers.java
@@ -1,17 +1,17 @@
 
 public class sortHelpers {
 
-	public static int[] merge(int[]list1,int[]list2)
-	{int[]merged=new int[list1.length+list2.length];
+	public static String[] merge(String[]list1,String[]list2)
+	{String[]merged=new String[list1.length+list2.length];
 	  for(int n=0;n<merged.length;n++)
 	  {int l=0;
 	   int r=0;
 	   
-	      {if((l<list1.length)&&(r<list2.length)&&(list1[l]<list2[r]))
+	      {if((l<list1.length)&&(r<list2.length)&&(list1[l].compareTo(list2[r])<0))
 	    	{merged[n]=list1[l];
 	        l++;}
 	      }
-	      {if((r<list2.length)&&(r<list2.length)&&(list1[l]>list2[r]))
+	      {if((r<list2.length)&&(r<list2.length)&&(list1[l].compareTo(list2[r])>0))
 	      {merged[n]=list2[r];  
 	       r++;
 	      }
@@ -32,12 +32,23 @@ public class sortHelpers {
 	  return merged;
 }
 	public static int partition(int[]list1)
-	{for(int n=1;n<list1.length;n++)
-	 {if (list1[n]<list1[0])
-	   {
-		 
-	   }
-		
+	{int[]ordered=new int[list1.length];
+	 int index=0;
+	for(int n=1;n<list1.length;n++)
+	     { int x=0;
+	       int y=list1.length;
+		   
+	       if (list1[n]<list1[0])
+	       {list1[n]=ordered[x];
+	       x++;}
+	       
+	       if (list1[n]>list1[0])
+		  {list1[n]=ordered[y];
+		   y--;}
+	     index=x+1;	 
+	     }
+	 return index;
 	 }
-	}
+	
 }
+
